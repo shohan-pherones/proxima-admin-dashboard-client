36ea93892ec55f39775828c57e3a4d1c686acb7f
shohan-pherones <shohan.sub.56@gmail.com>
2023-01-17 19:33:50 +0600

    redux and mui setup done

---
+++ src/theme.js
@@ -0,0 +1,139 @@
+// color design tokens export
+export const tokensDark = {
+  grey: {
+    0: "#ffffff", // manually adjusted
+    10: "#f6f6f6", // manually adjusted
+    50: "#f0f0f0", // manually adjusted
+    100: "#e0e0e0",
+    200: "#c2c2c2",
+    300: "#a3a3a3",
+    400: "#858585",
+    500: "#666666",
+    600: "#525252",
+    700: "#3d3d3d",
+    800: "#292929",
+    900: "#141414",
+    1000: "#000000", // manually adjusted
+  },
+  primary: {
+    // dark green
+    100: "#ccd6d2",
+    200: "#9aaca6",
+    300: "#678379",
+    400: "#35594d",
+    500: "#023020",
+    600: "#02261a",
+    700: "#011d13",
+    800: "#01130d",
+    900: "#000a06",
+  },
+  secondary: {
+    // light green
+    50: "#f0f0f0", // manually adjusted
+    100: "#e9fce9",
+    200: "#d3f8d3",
+    300: "#bcf5bc",
+    400: "#a6f1a6",
+    500: "#90ee90",
+    600: "#73be73",
+    700: "#568f56",
+    800: "#3a5f3a",
+    900: "#1d301d",
+  },
+};
+
+// function that reverses the color palette
+function reverseTokens(tokensDark) {
+  const reversedTokens = {};
+  Object.entries(tokensDark).forEach(([key, val]) => {
+    const keys = Object.keys(val);
+    const values = Object.values(val);
+    const length = keys.length;
+    const reversedObj = {};
+    for (let i = 0; i < length; i++) {
+      reversedObj[keys[i]] = values[length - i - 1];
+    }
+    reversedTokens[key] = reversedObj;
+  });
+  return reversedTokens;
+}
+export const tokensLight = reverseTokens(tokensDark);
+
+// mui theme settings
+export const themeSettings = (mode) => {
+  return {
+    palette: {
+      mode: mode,
+      ...(mode === "dark"
+        ? {
+            // palette values for dark mode
+            primary: {
+              ...tokensDark.primary,
+              main: tokensDark.primary[400],
+              light: tokensDark.primary[400],
+            },
+            secondary: {
+              ...tokensDark.secondary,
+              main: tokensDark.secondary[300],
+            },
+            neutral: {
+              ...tokensDark.grey,
+              main: tokensDark.grey[500],
+            },
+            background: {
+              default: tokensDark.primary[600],
+              alt: tokensDark.primary[500],
+            },
+          }
+        : {
+            // palette values for light mode
+            primary: {
+              ...tokensLight.primary,
+              main: tokensDark.grey[50],
+              light: tokensDark.grey[100],
+            },
+            secondary: {
+              ...tokensLight.secondary,
+              main: tokensDark.secondary[600],
+              light: tokensDark.secondary[700],
+            },
+            neutral: {
+              ...tokensLight.grey,
+              main: tokensDark.grey[500],
+            },
+            background: {
+              default: tokensDark.grey[0],
+              alt: tokensDark.grey[50],
+            },
+          }),
+    },
+    typography: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 12,
+      h1: {
+        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Inter", "sans-serif"].join(","),
+        fontSize: 14,
+      },
+    },
+  };
+};
