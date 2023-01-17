3da946e8acb894352af1d4be9517a201a7bbcf4b
shohan-pherones <shohan.sub.56@gmail.com>
2023-01-17 20:07:48 +0600

    app bar left added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import createTheme from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "theme";
 import { useSelector } from "react-redux";
 import { useMemo } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Layout from "components/layout";
+import Dashboard from "components/dashboard";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -10,9 +13,17 @@ function App() {
 
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
